10756db2d585ba72ce007b89dd342eafde83fd62
AshBll <61969618+AshBll@users.noreply.github.com>
2020-03-16 10:35:13 +0100

    Add index.html and script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const myFavoriteMovie = 'The Lord of the Rings'
+const releaseDate = '2001'
+const director = 'Peter Jackson'
+
+
+alert(myFavoriteMovie + ', réalisé par ' + director + ', est sorti en ' + releaseDate);
